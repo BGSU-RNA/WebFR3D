179620cf16fff2972ab948b70e205676c8d5251c
Blake Sweeney <blakes.85@gmail.com>
2014-10-13 16:03:09 -0400

    Reorganize to be a flask app
    This server would be better off as a simple flask application. I have outlined
    the changes that need to be made here. Most of the required functions have been
    stubbed out and some templates created. Hopefully this will make creating a
    flask app that uses python background workers easier.

---
+++ app.py
@@ -0,0 +1,65 @@
+import json
+
+from flask import Flask
+from flask import request
+from flask import g
+from flask import redirect
+from flask import url_for
+
+import db
+import utils
+
+app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    g.db = db.connect()
+    if not hasattr(g, 'beanstalk'):
+        g.beanstalk = db.beanstalk()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    data = db.default_info(g.db)
+    data['given'] = request.args
+    if request.method == 'POST':
+        data['given'] = request.form
+    return render_template('index.html', data=data)
+
+
+@app.route("/search", methods=['POST'])
+def search():
+    data = request.form
+    try:
+        data = utils.santize_search(data)
+        db.store(g.db, data)
+        g.beanstalk.put(json.dumps(data))
+        return redirect(url_for('results', id=data['id']))
+    except utils.ValidationError:
+        return render_template("invalid.html")
+    except:
+        return render_template('failed.html')
+
+
+@app.route("/results/:id", method=['GET'])
+def result(id):
+    data = db.result(g.db, id)
+    if data['status'] == 'complete':
+        return render_template('results/complete.html', data=data)
+    if data['status'] == 'pending':
+        return render_template('results/pending.html', data=data)
+    if data['status'] == 'failed':
+        render_template('results/failed.html', data=data)
+    return render_template('unknown.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ utils.py
@@ -0,0 +1,8 @@
+class ValidationError(Exception):
+    """Raised when a search is invalid or cannot be santized.
+    """
+    pass
+
+
+def santize_search(search):
+    pass
